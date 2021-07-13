564ff5752dea8bf9c55b16e2ed4ad29849df14b4
Nativ Lengel <Nativ.Lengel@wdc.com>
2021-07-13 14:51:41 +0300

    started

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const api = require('./routes/api');
 const app = express();
 const cors = require('cors');
 const port = 3000
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors);
-app.use('/api', api);
 app.get('/', (req, res) => {
     res.send("main")
 })
+app.use('/api', api);
 app.listen(port, () => {
     console.log(`http://localhost:${port}/api`);
 })
