da52f94fb4fa9f60b8da0533514ec080f470a629
Nativ Lengel <Nativ.Lengel@wdc.com>
2021-07-14 10:25:11 +0300

    [add] /frontend : angular project

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const api = require('./routes/api');
 const app = express();
+const path = require('path')
 const cors = require('cors');
 const port = 3000
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send("main")
+    res.sendFile('../frontend/index.html')
 })
 app.use('/api', api);
 app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
     console.log(`http://localhost:${port}/api`);
 })
